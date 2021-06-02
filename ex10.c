0f8b5243e620861f661158b87c5eb2a2095b8537
Nicolás Arias <55033621+SrDeWitt1912@users.noreply.github.com>
2021-06-02 17:30:59 -0500

    Create ex10.c

--- ex10.c
+++ ex10.c
@@ -4,6 +4,11 @@ int main(int argc, char *argv[])
 {
 	int i = 0;
 
+	for (i = 1; i < argc; i++)
+	{
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+
 	// let's make our own array of strings
 	char *states[] =
 	{
